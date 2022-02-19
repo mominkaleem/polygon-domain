ae9c015e0da5e9be586bbeaa5a2cbf20e9c7c931
Momin Kaleem <mominkaleem6@gmail.com>
2022-02-18 21:45:05 -0500

    mmake main actions of ENS

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,20 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
-contract Domains{
+contract Domains {
+		// Here's our domain TLD!
+    string public tld;
 
-    mapping(string => address) public domains;
-    mapping(string => string) public records;
-
-    constructor() {
-        console.log("THIS IS MY DOMAIN CONTRACT. NICE.");
+	  mapping(string => address) public domains;
+	  mapping(string => string) public records;
+		
+		// We make the contract "payable" by adding this to the constructor
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
+		
+		// This function will give us the price of a domain based on length
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+	        return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals) 
+        } else if (len == 4) {
+	        return 3 * 10**17; // To charge smaller amounts, reduce the decimals
+        } else {
+	        return 1 * 10**17;
+        }
     }
 
-    function register(string calldata name) public {
+    function register(string calldata name) public payable{
         require(domains[name] == address(0));
+        
+        uint _price = this.price(name);
+
+				// Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,30 +1,30 @@
 const main = async () => {
-	const [owner, randomPerson] = await hre.ethers.getSigners();
-  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-	const domainContract = await domainContractFactory.deploy();
-	await domainContract.deployed();
-  console.log("Contract deployed to:", domainContract.address);
-	console.log("Contract deployed by:", owner.address);
-	
-	let txn = await domainContract.register("zralkh");
-	await txn.wait();
-
-  const domainAddress = await domainContract.getAddress("zralkh");
-  console.log("Owner of domain zralkh:", domainAddress);
-	
-	// Trying to set a record that doesn't belong to me!
-  txn = await domainContract.connect(owner).setRecord("zralkh", "Haha my domain now!");
-  await txn.wait();
-}
-
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+      // We pass in "ninja" to the constructor when deploying
+      const domainContract = await domainContractFactory.deploy("ninja");
+      await domainContract.deployed();
+  
+    console.log("Contract deployed to:", domainContract.address);
+      
+      // We're passing in a second variable - value. This is the moneyyyyyyyyyy
+      let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+      await txn.wait();
+  
+    const address = await domainContract.getAddress("mortal");
+    console.log("Owner of domain mortal:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
   }
-};
-
-runMain();
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
