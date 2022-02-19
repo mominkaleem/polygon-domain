9b7b40953388e3da0d700cd4f10b8c8cf2d19982
Momin Kaleem <mominkaleem6@gmail.com>
2022-02-18 22:28:12 -0500

    make NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,17 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
       // We pass in "ninja" to the constructor when deploying
-      const domainContract = await domainContractFactory.deploy("ninja");
+      const domainContract = await domainContractFactory.deploy("beedu");
       await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
       
       // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-      let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+      let txn = await domainContract.register("mo",  {value: hre.ethers.utils.parseEther('0.1')});
       await txn.wait();
   
-    const address = await domainContract.getAddress("mortal");
-    console.log("Owner of domain mortal:", address);
+    const address = await domainContract.getAddress("mo");
+    console.log("Owner of domain mo:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
